992c8fd557ec85fa0df4f0d6ef24d478c73abf08
Wilson <wilson.omokoro@andela.com>
2015-11-09 12:50:43 +0100

    Write tests

---
+++ tests/BaseTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wilson\tests;
+
+use Wilson\Source\Base;
+use PHPUnit_Framework_TestCase;
+
+class BaseTest extends PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/ConnectionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wilson\tests;
+
+use Wilson\Source\Connection;
+use PHPUnit_Framework_TestCase;
+
+class ConnectionTest extends PHPUnit_Framework_TestCase
+{
+    public function testConnect()
+    {
+
+    }
+}
