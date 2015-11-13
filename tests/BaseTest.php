6c86d44be856cdde2cd268ad489c61a98c8afcf8
Wilson <wilson.omokoro@andela.com>
2015-11-13 12:21:39 +0100

    Comment BaseTest class properly

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -9,6 +9,9 @@ use Wilson\Source\Examples\User;
 
 class BaseTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Test that the right table name can be constructed from the class name
+     */
     public function testGetTableName()
     {
         $table = User::getTableName();
@@ -16,18 +19,27 @@ class BaseTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("users", $table);
     }
 
+    /**
+     * Test that the find() method can actually find a record in the database
+     */
     public function testFind()
     {
         $mock = Mockery::mock('Wilson\Source\Examples\User');
         $mock->shouldReceive('find')->with(1)->andReturn(new static);
     }
 
+    /**
+     * Test that the save() method can actually save or update a record in the database table
+     */
     public function testSave()
     {
         $mock = Mockery::mock('Wilson\Source\Examples\User');
         $mock->shouldReceive('save')->once()->andReturn('1');
     }
 
+    /**
+     * Test that the correct update SQL can be dynamically generated when updating a record
+     */
     public function testMakeUpdateSQL()
     {
         $user = new User();
@@ -40,6 +52,9 @@ class BaseTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $actual);
     }
 
+    /**
+     * Test that the destroy() method can actually delete a record from the database table
+     */
     public function testDestroy()
     {
         $mock = Mockery::mock('Wilson\Source\Examples\User');
